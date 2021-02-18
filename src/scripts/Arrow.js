2bcc3a5c615894645107d9c51082a42b065c59d7
Rita <ritadecassia.infor2@gmail.com>
2021-02-18 12:09:48 -0300

    Authetication project add

---
+++ src/scripts/Arrow.js
@@ -0,0 +1,5 @@
+var buttonArrowDown = document.querySelector("#buttonArrowDown");
+
+buttonArrowDown.addEventListener("click", () => {
+	buttonArrowDown.innerHTML = "<i class="fas fa-angle-up"></i>"
+})
