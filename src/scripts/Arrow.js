573e662c00bf14e4acf420edf2f359a0746240cd
Rfont <ritadecassia.infor2@gmail.com>
2021-02-27 13:06:20 -0300

    Layout das páginas com os projetos criado

--- src/scripts/Arrow.js
+++
@@ -1,5 +0,0 @@
-var buttonArrowDown = document.querySelector("#buttonArrowDown");
-
-buttonArrowDown.addEventListener("click", () => {
-	buttonArrowDown.innerHTML = "<i class="fas fa-angle-up"></i>"
-})
